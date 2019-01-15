180bb30282a468efee97e6ac30c8ea371b1a8d5e
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-15 16:21:19 +0000

    Number to Month

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,7 @@ end
 def add_string_as_number(string_1,string_2)
   return string_1.to_i + string_2.to_i
 end
+
+def number_to_full_month_name(number_to_short_month_name)
+  return "January" if number_to_short_month_name ==1
+end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -47,11 +47,11 @@ class FunctionsTest < MiniTest::Test
     assert_equal( 3, add_result )
   end
 
-  # def test_number_to_full_name__month_1()
-  #   result = number_to_full_month_name( 1 )
-  #   assert_equal( "January", result )
-  # end
-  #
+  def test_number_to_full_name__month_1()
+    result = number_to_full_month_name( 1 )
+    assert_equal( "January", result )
+  end
+
   # def test_number_to_full_name__month_3()
   #   result = number_to_full_month_name( 3 )
   #   assert_equal( "March", result )
